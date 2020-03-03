082f2b019af8788323e5f0cdc20822a235cb84aa
EngiGu <sayheya@qq.com>
2020-03-03 10:08:55 +0800

    更新api

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import hashlib
 import base64
-import aiohttp
+import time
 from sanic import Sanic
 from sanic.response import json as sanic_json
+from sanic import response
 
 from config import Config
 from core.model import SQLiteModel
@@ -12,51 +13,41 @@ app = Sanic(__name__)
 SQLITE_MODEL = SQLiteModel()
 
 
-def msg(code=0, msg='ok!', url=''):
-    return sanic_json({'code': code, 'msg': msg, 'url': url})
+def msg(code=0, msg='ok!', data=''):
+    return sanic_json({'code': code, 'msg': msg, 'data': data})
 
 
-@app.route("/api/upload", methods=['POST'])
+@app.route('/')
+async def handle_request(request):
+    return await response.file('static/index.html')
+
+
+@app.route('/favicon.ico')
+async def handle_request(request):
+    return await response.file('static/favicon.ico')
+
+
+@app.route("/api/total_status", methods=['GET'])
 async def upload(request):
-    try:
-        # 文件上传
-        pic_file = request.files.get('file')  # type body name
-        # 文件类型校验
-        if Config.ONLY_UPLOAD_IMG_FILES and 'image' not in pic_file.type:
-            return msg(code=-1, msg='请务必只上传图片文件！')
+    total, last_pub_time, url_obj = SQLITE_MODEL.get_total_status()
+    data = {
+        'total': total,
+        'last_pub_time': last_pub_time.strftime("%Y-%m-%d"),
+        '_last_pub_time': last_pub_time,
+        'title': url_obj.title,
+        'url': url_obj.url,
+    }
+    return msg(data=data)
 
-        upload_way = request.form.get('uploadWay') or ''
-        upload_way = upload_way.lower()
 
-        # upload_way 校验
-        if upload_way not in SUPPORT_UPLOAD_WAYS or not upload_way:
-            return msg(code=-1, msg='uploadWay错误！', url='uploadWay错误！')
+@app.route("/api/last_pub_time_cheaters/<stmp:int>", methods=['GET'])
+async def upload(request, stmp):
+    stmp = time.strftime("%Y-%m-%d", time.localtime(stmp))
+    cheaters = SQLITE_MODEL.get_last_pub_time_cheaters(stmp)
+    data = [Utils.row2dict(i) for i in cheaters]
+    return msg(data=data)
 
-        # uploader 获取
-        uploader = __UPLODER_MAPS__.get(upload_way, None)
-        if not uploader:
-            return msg(code=-1, msg='找不到对用的uploder！', url='找不到对用的uploder！')
-
-        file_hash = await uploader.generate_file_hash(pic_file.body)
-        raw_suffix = pic_file.name.split('.')[-1]
-        file_name = '%s.%s' % (file_hash, raw_suffix)
-
-        record = SQLITE_MODEL.get_one_record(name=file_name, upload_way=upload_way)
-        if record:
-            # 之前有记录
-            return msg(msg='之前上传过哟～', url=await uploader.format_pic_url(file_name))
-
-        # 上传
-        result = await uploader.upload(file=pic_file.body, filename=file_name, raw_filename=pic_file.name)
-        code, show_msg, url, need_add_record = await uploader.deal_upload_result(result, filename=file_name)
-        if need_add_record:
-            # 添加记录
-            SQLITE_MODEL.add_one_record(name=file_name, upload_way=upload_way)
-
-        return msg(code=code, msg=show_msg, url=url)
-    except Exception as e:
-        show_msg = '内部错误！error：%s' % str(e)
-        return msg(code=-1, msg=show_msg, url=show_msg)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=Config.API_SERVER_PORT, workers=Config.API_SERVER_WORKERS)
+    app.run(host="0.0.0.0", port=Config.API_SERVER_PORT,
+            workers=Config.API_SERVER_WORKERS)
--- core/model.py
+++ core/model.py
@@ -19,7 +19,8 @@ class SQLiteModel:
             if url_obj:
                 return url_obj
 
-            url_obj = Urls(url=url, title=title, created=Utils.now(return_datetime=False))
+            url_obj = Urls(url=url, title=title,
+                           created=Utils.now(return_datetime=False))
             s.add(url_obj)
             return url_obj
 
@@ -27,7 +28,8 @@ class SQLiteModel:
         with session_scope() as s:
             ch_ex = self.get_one_cheater(name, b_id)
             if not ch_ex:
-                ch = Cheaters(name=name, b_id=b_id, u_id=u_id, created=Utils.now(return_datetime=False), pub_time=pub_time)
+                ch = Cheaters(name=name, b_id=b_id, u_id=u_id, created=Utils.now(
+                    return_datetime=False), pub_time=pub_time)
                 s.add(ch)
                 return ch
             return ch_ex
@@ -47,3 +49,26 @@ class SQLiteModel:
             obj.status = status
             s.flush()
 
+    # api
+
+    def get_total_status(self):
+        with session_scope() as s:
+            total = s.query(func.count(Cheaters.id)).scalar()
+
+            last_pub_time, url_obj = s.query(Cheaters.pub_time, Urls).join(
+                Urls,
+                Urls.id == Cheaters.u_id
+            ).group_by(
+                Cheaters.pub_time
+            ).order_by(
+                Cheaters.pub_time.desc()
+            ).limit(1).first()
+            # print(total, last_pub_time, url_obj)
+            return total, last_pub_time, url_obj
+
+    def get_last_pub_time_cheaters(self, pub_time):
+        with session_scope() as s:
+            return s.query(Cheaters).filter(
+                Cheaters.pub_time >= '%s 00:00:00' % pub_time,
+                Cheaters.pub_time <= '%s 23:59:59' % pub_time,
+            ).limit(100).all()
